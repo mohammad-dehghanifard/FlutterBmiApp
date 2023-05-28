43c887366a5b4070909dcf052d69c7e98f6f8e20
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-05-28 02:09:26 -0700

    add dependency

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,18 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: HomeScreen(),
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
 
