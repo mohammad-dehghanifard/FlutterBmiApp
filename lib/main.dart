02933bd35cb329b130a593559d4402a363dff1f0
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-06-03 08:05:27 -0700

    create result screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bmi/features/result_features/screens/result_screen.dart';
 
-import 'features/home_features/screeens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: const ResultScreen(),
     );
   }
 }
